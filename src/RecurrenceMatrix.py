15dae689ae8ae88663528c01fb3c150730f0cc3a
jfriend08 <ys486@cornell.edu>
2016-04-01 10:37:18 -0400

    kernal

--- src/RecurrenceMatrix.py
+++ src/RecurrenceMatrix.py
@@ -11,10 +11,42 @@ def construct(labels):
   return R
 
 def adjacentMatrix(size):
-  res = np.array([ [0]*size for _ in xrange(size)])
+  res = np.array([ [0.0]*size for _ in xrange(size)])
   for i in xrange(size):
     if i-1 >=0:
       res[i][i-1] = 1
     if i+1 < size:
       res[i][i+1] = 1
   return res
+
+def sumEachRow(m):
+  r, c = m.shape
+  res = np.zeros(r)
+  for i in xrange(r):
+    res[i] = m[i,:]
+  return res
+
+
+def getBalancedMiu(R, delta):
+  d_R = np.apply_along_axis(sum, 1, R)
+  d_delta = np.apply_along_axis(sum, 1, delta)
+  return np.dot(d_R,d_R+d_delta)/np.power(np.linalg.norm(d_R + d_delta), 2)
+
+def gaussianKernel(m, c=0.5):
+  numSample, numFeature = m.shape
+  res = np.array([ [0.0]*numSample for _ in xrange(numSample)])
+  for i in xrange(numSample):
+    for j in xrange(i, numSample):
+      diff = np.power(np.linalg.norm(m[i,:] - m[j,:]), 2)
+      val = np.exp((-1*diff/(2*c**2)))
+      res[i][j] = val
+      res[j][i] = val
+  return res
+
+# delta = adjacentMatrix(10)
+# res = gaussianKernel(delta)
+# print res
+
+
+
+
