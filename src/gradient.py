2c52dce756623b21c8edec5b61e9e669f25c4d4d
jfriend08 <ys486@cornell.edu>
2016-04-15 14:34:27 -0400

    numerical vs analytical

--- src/gradient.py
+++ src/gradient.py
@@ -20,9 +20,9 @@ def getLaplacianMatrix(m):
 def numericalGradient(m, x, y):
   limx, limy = m.shape
   if x > limx or y > limy:
-    raise ValueError('position outside matrix')
+    raise ValueError('Position outside matrix')
   if x == y:
-    return m
+    raise ValueError('Cannot no self weighting')
   d = 1e-6
   f1, f2 = np.copy(m), np.copy(m)
   f1[x,y] += d
@@ -30,7 +30,39 @@ def numericalGradient(m, x, y):
   f2[x,y] -= d
   f2[y,x] -= d
   l1, l2 = getLaplacianMatrix(f1), getLaplacianMatrix(f2)
+  # L1, d1= scipy.sparse.csgraph.laplacian(f1, normed=True, return_diag=True)
+  # L2, d2= scipy.sparse.csgraph.laplacian(f2, normed=True, return_diag=True)
   return (l1-l2)/(2*d)
+
+def analyticalGradient(m, x, y):
+  limx, limy = m.shape
+  if x > limx or y > limy:
+    raise ValueError('Position outside matrix')
+  if x == y:
+    raise ValueError('Cannot no self weighting')
+
+  g = np.array(m)
+  np.fill_diagonal(g, 0)
+  d = g.sum(axis = 1)
+
+  res = np.array(m)
+  res.fill(0)
+  for i in xrange(limx):
+    for j in xrange(i+1, limy):
+      if (i==x and j==y) or (i==y and j==x):
+        val = -1/(d[i]*d[j])**0.5 + m[i,j]*(d[i]+d[j])/(2*(d[i]*d[j])**1.5)
+        res[i,j] = val
+        res[j,i] = val
+      elif i==x or i==y:
+        val =  m[i,j]/(2*(d[i]**1.5*d[j]**0.5))
+        res[i,j] = val
+        res[j,i] = val
+      elif j==x or j==y:
+        val =  m[i,j]/(2*(d[j]**1.5*d[i]**0.5))
+        res[i,j] = val
+        res[j,i] = val
+  return res
+
 
 m = np.array([[1,2,3,4,5],[2,1,2,3,4],[3,2,1,1,1],[4,3,1,1,1],[5,4,1,1,1]]).astype(float)
 # m = np.ones((5,5))
@@ -39,4 +71,6 @@ m = np.array([[1,2,3,4,5],[2,1,2,3,4],[3,2,1,1,1],[4,3,1,1,1],[5,4,1,1,1]]).asty
 
 # L, d= scipy.sparse.csgraph.laplacian(m, normed=True, return_diag=True)
 # print L
-numericalGradient(m, 1,2)
+dL_num = numericalGradient(m, 1,2)
+dL_ana = analyticalGradient(m, 1,2)
+print "all diff are below 1e-10: ", np.all((dL_num-dL_ana)<1e-10)
