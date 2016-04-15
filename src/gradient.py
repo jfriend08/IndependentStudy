ac80a3057835c22511ef40695dbe43fc383af03c
jfriend08 <ys486@cornell.edu>
2016-04-15 13:52:59 -0400

    numerical gradient

--- src/gradient.py
+++ src/gradient.py
@@ -17,11 +17,26 @@ def getLaplacianMatrix(m):
   np.fill_diagonal(res, 1)
   return res
 
+def numericalGradient(m, x, y):
+  limx, limy = m.shape
+  if x > limx or y > limy:
+    raise ValueError('position outside matrix')
+  if x == y:
+    return m
+  d = 1e-6
+  f1, f2 = np.copy(m), np.copy(m)
+  f1[x,y] += d
+  f1[y,x] += d
+  f2[x,y] -= d
+  f2[y,x] -= d
+  l1, l2 = getLaplacianMatrix(f1), getLaplacianMatrix(f2)
+  return (l1-l2)/(2*d)
 
-m = np.array([[1,2,3,4,5],[2,1,2,3,4],[3,2,1,1,1],[4,3,1,1,1],[5,4,1,1,1]])
+m = np.array([[1,2,3,4,5],[2,1,2,3,4],[3,2,1,1,1],[4,3,1,1,1],[5,4,1,1,1]]).astype(float)
 # m = np.ones((5,5))
-print m
-print getLaplacianMatrix(m)
+# print m
+# print getLaplacianMatrix(m)
 
-L, d= scipy.sparse.csgraph.laplacian(m, normed=True, return_diag=True)
-print L
+# L, d= scipy.sparse.csgraph.laplacian(m, normed=True, return_diag=True)
+# print L
+numericalGradient(m, 1,2)
